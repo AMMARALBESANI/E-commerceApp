e204ac954b6e32d86ee0bb74c65dccee2d11e574
Ammar <100584308+AMMARALBESANI@users.noreply.github.com>
2023-09-20 23:47:45 +0300

    cookies

---
+++ E-Commerce-App/Components/AddProductToCartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce_App.Models;
+using E_Commerce_App.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace E_Commerce_App.Components
+{
+	public class AddProductToCartViewComponent : ViewComponent
+    {
+		private readonly IProduct _product;
+
+        public AddProductToCartViewComponent(IProduct product)
+        {
+            _product= product;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int ProductId)
+		{
+			var CookiesData=HttpContext.Request.Cookies["CartCookie"];
+			List<int> ProductIds;
+			if (CookiesData != null) 
+			{
+                ProductIds = JsonConvert.DeserializeObject<List<int>>(CookiesData);
+
+			}
+			else
+			{
+				ProductIds = new List<int>();
+
+			}
+			ProductIds.Add(ProductId);
+			var options = new CookieOptions
+			{
+				Expires = DateTime.Now.AddDays(7),
+
+			};
+            HttpContext.Response.Cookies.Append("CartCookie", JsonConvert.SerializeObject(ProductIds), options);
+            return View();
+		}
+	}
+}
---
+++ E-Commerce-App/Components/CartViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using E_Commerce_App.Models;
+using E_Commerce_App.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace E_Commerce_App.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IProduct _product;
+
+        public CartViewComponent(IProduct product)
+        {
+            _product = product;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var CookiesData = HttpContext.Request.Cookies["CartCookie"];
+           
+            if (CookiesData != null)
+            {
+               var ProductIds = JsonConvert.DeserializeObject<List<int>>(CookiesData);
+                List<Product> Product = new List<Product>();
+                foreach (var item in ProductIds)
+                {
+                    await _product.GetProductAsync(item);
+                    
+                }
+               
+            }
+            else
+            {
+                return Content("");
+
+            }
+
+        }
+    }
+}
