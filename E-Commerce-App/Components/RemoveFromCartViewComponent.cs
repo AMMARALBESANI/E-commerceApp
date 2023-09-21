eb9c79d0aa59bf3b13d04df8e135d4600b50b986
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-09-21 05:13:23 +0300

    Done still need design (A lot of design)

---
+++ E-Commerce-App/Components/RemoveFromCartViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using E_Commerce_App.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace E_Commerce_App.Components
+{
+    public class RemoveFromCartViewComponent : ViewComponent
+    {
+        private readonly IProduct _product;
+        public RemoveFromCartViewComponent(IProduct product)
+        {
+            _product = product;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int productID)
+        {
+            var CookiesData = HttpContext.Request.Cookies["CartCookie"];
+            List<int> ProductIds;
+            if (CookiesData != null)
+            {
+                ProductIds = JsonConvert.DeserializeObject<List<int>>(CookiesData);
+                if (ProductIds.Contains(productID))
+                {
+                    ProductIds.Remove(productID);
+                    var options = new CookieOptions
+                    {
+                        Expires = DateTime.Now.AddDays(7),
+
+                    };
+                    HttpContext.Response.Cookies.Append("CartCookie", JsonConvert.SerializeObject(ProductIds), options);
+                }
+            }
+
+
+            return View();
+        }
+    }
+}
